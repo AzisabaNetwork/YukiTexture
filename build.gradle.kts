73b015efb048442a932041be38aad6922a107bab
sysnote8main <sysnote8main@gmail.com>
2025-03-05 21:41:12 +0900

    feat: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,24 @@
 plugins {
     kotlin("jvm") version "1.7.21"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.gradleup.shadow") version "9.0.0-beta8"
 }
 
-group = "net.azisaba"
-version = "3.0.2"
+group = "net.azisaba.yukitexture"
+version = "3.1.0-1.21.1"
 
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(8))
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 
 repositories {
     mavenCentral()
     maven("https://jitpack.io/")
-    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://rayzr.dev/repo/")
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
-    implementation("redis.clients:jedis:4.3.2")
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
+    implementation("redis.clients:jedis:5.2.0")
     implementation("com.github.kittinunf.fuel:fuel:2.2.3")
     implementation("commons-codec:commons-codec:1.15")
 }
