4f50cbbed06f1a42f7b7303a41630167a91052a3
sysnote8main <sysnote8main@gmail.com>
2025-03-08 17:55:30 +0900

    feat: add global logger

---
+++ src/main/kotlin/net/azisaba/yukitexture/Variables.kt
@@ -0,0 +1,8 @@
+package net.azisaba.yukitexture
+
+import org.slf4j.LoggerFactory
+
+internal const val PL_NAME = "YukiTexture"
+internal const val PL_ID = "yukitexture"
+
+internal val LOGGER = LoggerFactory.getLogger(PL_NAME)
