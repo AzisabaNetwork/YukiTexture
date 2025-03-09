dd073850261500163248d7aef3084fea9cd4be82
sysnote8main <sysnote8main@gmail.com>
2025-03-09 10:30:54 +0900

    temp commit

--- src/main/kotlin/net/azisaba/yukitexture/merger/ResourcePackMerger.kt
+++ src/main/kotlin/net/azisaba/yukitexture/merger/ResourcePackMerger.kt
@@ -12,51 +12,50 @@ import java.nio.file.Path
 import kotlin.time.Duration
 
 class ResourcePackMerger {
-    suspend fun uploadToCloudflareR2(targetFilePath: Path) {
+    suspend fun uploadToCloudflareR2(targetFilePath: Path): Result<Url> {
         val bucketName = Credentials.BUCKET_NAME
         val objectKey = Credentials.OBJECY_KEY // アップロードするファイル名
         val cloudflareEndpoint = Credentials.CF_ENDPOINT
 
-        val s3Client =
-            S3Client {
-                region = Credentials.REGION
-                endpointUrl = Url.parse(cloudflareEndpoint)
-                credentialsProvider =
-                    StaticCredentialsProvider {
-                        accessKeyId = Credentials.ACCESS_KEY_ID
-                        secretAccessKey = Credentials.ACCESS_SECRET_KEY
+        S3Client {
+            region = Credentials.REGION
+            endpointUrl = Url.parse(cloudflareEndpoint)
+            credentialsProvider =
+                StaticCredentialsProvider {
+                    accessKeyId = Credentials.ACCESS_KEY_ID
+                    secretAccessKey = Credentials.ACCESS_SECRET_KEY
+                }
+        }.use { s3Client ->
+
+            try {
+                val putObjectRequest =
+                    PutObjectRequest {
+                        bucket = bucketName
+                        key = objectKey
+                        body = ByteStream.fromBytes(Files.readAllBytes(targetFilePath))
                     }
+
+                s3Client.putObject(putObjectRequest)
+                println("File uploaded successfully: $objectKey")
+
+                val getObjectRequest =
+                    GetObjectRequest {
+                        bucket = bucketName
+                        key = objectKey
+                    }
+
+                // URL を生成
+                val presignedUrl =
+                    s3Client.presignGetObject(
+                        getObjectRequest,
+                        duration = Duration.parse("7d"),
+                    )
+                println("File URL: ${presignedUrl.url}")
+                return Result.success(presignedUrl.url)
+            } catch (e: Exception) {
+                println("Upload failed: ${e.message}")
+                return Result.failure(e)
             }
-
-        try {
-            val putObjectRequest =
-                PutObjectRequest {
-                    bucket = bucketName
-                    key = objectKey
-                    body = ByteStream.fromBytes(Files.readAllBytes(targetFilePath))
-                }
-
-            s3Client.putObject(putObjectRequest)
-            println("File uploaded successfully: $objectKey")
-
-            val getObjectRequest =
-                GetObjectRequest {
-                    bucket = bucketName
-                    key = objectKey
-                }
-
-            // URL を生成
-            val presignedUrl =
-                s3Client.presignGetObject(
-                    getObjectRequest,
-                    duration = Duration.parse("7d"),
-                )
-            println("File URL: ${presignedUrl.url}")
-        } catch (e: Exception) {
-            println("Upload failed: ${e.message}")
-            e.printStackTrace()
-        } finally {
-            s3Client.close()
         }
     }
 }
