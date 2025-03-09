013b9911270c7ed5482cfa04a4dddeebfdc98845
sysnote8main <sysnote8main@gmail.com>
2025-03-09 13:15:58 +0900

    chore: texture reload command is disabled temporary

--- src/main/kotlin/net/azisaba/yukitexture/command/ReloadTextureCommand.kt
+++ src/main/kotlin/net/azisaba/yukitexture/command/ReloadTextureCommand.kt
@@ -5,10 +5,18 @@ import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 
-class ReloadTextureCommand(private val plugin: YukiTexture) : CommandExecutor {
-
-    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
-        plugin.reloadTex(sender)
+class ReloadTextureCommand(
+    private val plugin: YukiTexture,
+) : CommandExecutor {
+    override fun onCommand(
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        args: Array<String>,
+    ): Boolean {
+//        plugin.reloadTex(sender)
+        // FIXME: able to use reload texture from command
+        sender.sendMessage("Temporary disabled.")
         return true
     }
 }
