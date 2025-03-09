bc0cf5f1fc87de737b0b056039b6dd2ed7f5f496
sysnote8main <sysnote8main@gmail.com>
2025-03-09 13:20:07 +0900

    feat: add util for git control easily

---
+++ src/main/kotlin/net/azisaba/yukitexture/util/GitUtil.kt
@@ -0,0 +1,41 @@
+package net.azisaba.yukitexture.util
+
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.api.PullResult
+import java.io.File
+
+class GitUtil {
+    fun update(targetFolder: File): Result<PullResult> {
+        return try {
+            val git = Git.open(targetFolder)
+            return Result.success(git.pull().call())
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    fun pull(
+        targetFolder: File,
+        remoteUrl: String,
+    ): Result<PullResult> {
+        if (!targetFolder.exists()) {
+            targetFolder.mkdirs()
+        }
+        try {
+            val result =
+                Git
+                    .open(targetFolder)
+                    .pull()
+                    .setRemote(remoteUrl)
+                    .call()
+
+            return if (result.isSuccessful) {
+                Result.success(result)
+            } else {
+                Result.failure(RuntimeException("Failed to pull."))
+            }
+        } catch (e: Exception) {
+            return Result.failure(e)
+        }
+    }
+}
